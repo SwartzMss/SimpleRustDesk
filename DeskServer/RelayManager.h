9a70ab1ada7098aac08ec1f208ffcb0d74ad013b
OverLoad <SwartzMss@users.noreply.github.com>
2025-03-12 10:17:13 +0800

    Create RelayManager.h

---
+++ DeskServer/RelayManager.h
@@ -0,0 +1,45 @@
+#ifndef RELAYMANAGER_H
+#define RELAYMANAGER_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QHostAddress>
+#include "rendezvous.pb.h"
+#include "ScreenCaptureEncoder.h"
+
+class RelayManager : public QObject {
+    Q_OBJECT
+public:
+    explicit RelayManager(QObject* parent = nullptr);
+    ~RelayManager();
+
+    // Start TCP connection to relay server and start capture/encoding process.
+    // 'uuid' and 'punchHoleId' are used for constructing the RequestRelay message.
+    void start(const QHostAddress& relayAddress, quint16 relayPort, const QString& uuid, const QString& punchHoleId);
+    // Stop TCP connection and stop capture/encoding.
+    void stop();
+
+signals:
+    void errorOccurred(const QString& errorString);
+    void connected();
+    void disconnected();
+
+private slots:
+    void onSocketConnected();
+    void onSocketDisconnected();
+    void onSocketError(QAbstractSocket::SocketError error);
+    // Receive encoded data from ScreenCaptureEncoder and forward it.
+    void onEncodedPacketReady(const QByteArray& packet);
+
+private:
+    QTcpSocket* m_socket;
+    QHostAddress m_relayAddress;
+    quint16 m_relayPort;
+    QString m_uuid;
+    QString m_punchHoleId;
+
+    // Screen capture and encoder instance managed internally.
+    ScreenCaptureEncoder* m_encoder;
+};
+
+#endif // RELAYMANAGER_H
