98a9095352b6287109e3ed520254eec93b6296e7
SwartzMss <swartz_lubel@outlook.com>
2025-03-14 22:42:29 +0800

    Fixes #9

--- RelayServer/main.cpp
+++ RelayServer/main.cpp
@@ -1,9 +1,24 @@
 #include "RelayServer.h"
 #include <QtWidgets/QApplication>
+#include <QSharedMemory>
+
+bool checkSingleInstance(const QString& key) {
+	static QSharedMemory sharedMem(key);
+	if (!sharedMem.create(1)) {
+		return false; // 已有实例在运行
+	}
+	return true; // 没有实例在运行
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+	const QString sharedMemoryKey = "RelayServerSharedMemory";
+
+	QSharedMemory sharedMem(sharedMemoryKey);
+	if (!sharedMem.create(1)) {
+		return 0;
+	}
 	RelayServer w;
     // 禁止最大化
     w.setWindowFlags(w.windowFlags() & ~Qt::WindowMaximizeButtonHint);
